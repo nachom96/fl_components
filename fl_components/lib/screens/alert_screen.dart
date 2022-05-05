c242e7a3e987985a2fc26cbe65874b3b62ef1a97
Nacho <44799327+nachom96@users.noreply.github.com>
2022-05-05 10:15:52 +0200

    Guardado

--- fl_components/lib/screens/alert_screen.dart
+++ fl_components/lib/screens/alert_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+        child: ElevatedButton(
+          // style: ElevatedButton.styleFrom(
+          //     primary: Colors.indigo,
+          //     shape: const StadiumBorder(), // Redondea los bordes
+          //     elevation: 0),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Mostrar alerta',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          onPressed: () {},
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.close),
+          backgroundColor: Colors.indigo,
+          onPressed: () {
+            Navigator.pop(context);
+          }),
     );
   }
 }
--- fl_components/lib/theme/app_theme.dart
+++ fl_components/lib/theme/app_theme.dart
@@ -4,13 +4,25 @@ class AppTheme {
   static const Color primary = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      //Color primario
+      // Color primario
       primaryColor: Colors.indigo,
 
-      //AppBar Theme
+      // AppBar Theme
       appBarTheme: AppBarTheme(color: primary, elevation: 0),
 
-      //TextButton Theme
+      // TextButton Theme
       textButtonTheme:
-          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)));
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
+
+      // FloatingActionButtons
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(backgroundColor: primary),
+
+      // ElevatedButtons
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            primary: Colors.indigo,
+            shape: const StadiumBorder(), // Redondea los bordes
+            elevation: 0),
+      ));
 }
